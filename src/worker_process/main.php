3d55363fa78556374f3a9ea21710b087b683c4de
rokugasenpai <rokugasenpai@yahoo.co.jp>
2015-04-15 00:22:21 +0900

    xdebugが有効だと時間がかかるので注意文言を追加

--- src/worker_process/main.php
+++ src/worker_process/main.php
@@ -2,7 +2,12 @@
 
 if ($argc != 3 || (string)(int)$argv[1] !== $argv[1] || (string)(int)$argv[2] !== $argv[2]) {
     echo('usage: main.php each times' . PHP_EOL);
-    echo('example: main.php 25000 4');
+    echo('example: main.php 25000 4' . PHP_EOL);
+    exit(1);
+}
+
+if (ini_get('xdebug.profiler_enable')) {
+    echo('maybe slow while xdebug is enabled' . PHP_EOL);
 }
 
 $each = (int)$argv[1];
