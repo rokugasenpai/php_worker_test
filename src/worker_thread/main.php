98e46849c2f3cc55a6041cdc3cfdb93c43a5377c
rokugasenpai <rokugasenpai@yahoo.co.jp>
2015-04-15 00:20:17 +0900

    Revert "xbudegが有効だと時間がかかるので注意文言を追加"
    This reverts commit 4ee176c7c4a69357462aa17adae8381f38842468.

--- src/worker_thread/main.php
+++ src/worker_thread/main.php
@@ -4,12 +4,7 @@ require_once(__DIR__ . DIRECTORY_SEPARATOR . 'worker_thread.php');
 
 if ($argc != 3 || (string)(int)$argv[1] !== $argv[1] || (string)(int)$argv[2] !== $argv[2]) {
     echo('usage: main.php each times' . PHP_EOL);
-    echo('example: main.php 25000 4' . PHP_EOL);
-    exit(1);
-}
-
-if (ini_get('xdebug.profiler_enable')) {
-    echo('maybe slow while xdebug is enabled' . PHP_EOL);
+    echo('example: main.php 25000 4');
 }
 
 $each = (int)$argv[1];
